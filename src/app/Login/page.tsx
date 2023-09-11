2953c92f5c3f681371a98ef89c76c74dbb6348d6
Vman384 <112357168+Vman384@users.noreply.github.com>
2023-09-11 21:09:10 +1000

    login fully working

--- src/app/Login/page.tsx
+++ src/app/Login/page.tsx
@@ -24,6 +24,7 @@ const Login: React.FunctionComponent<IPageProps> = props => {
             // Signed in 
             const user = userCredential.user;
             console.log("success")
+            window.location.href="/Menu"
             
           })
           .catch((error) => {
