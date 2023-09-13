3cf565f4c031b9c83662af5227e93dc04a9ca650
jwan0386 <140789279+jwan0386@users.noreply.github.com>
2023-09-13 12:40:33 +1000

    Filled in the table of the backlog and polished it a bit
    added in example entries for the product backlog. still the layout needs to be worked on but it is the foundation.

--- src/app/ProductBacklog/page.tsx
+++ src/app/ProductBacklog/page.tsx
@@ -4,10 +4,15 @@ import './App.css';
 
 const ProductBacklog: React.FunctionComponent<IPageProps> = props => {
     const data = [
-      {name:"John",as_a:"Dev",i_want_to:"",so_that:"",priority:"",sprint:"",status:""},
-      {name:"Harry",as_a:"SM",i_want_to:"",so_that:"",priority:"",sprint:"",status:""},
-      {name:"Josh",as_a:"proxy PO",i_want_to:"",so_that:"",priority:"",sprint:"",status:""},
-      {name:"Alex",as_a:"User",i_want_to:"",so_that:"",priority:"",sprint:"",status:""},
+      {date:"22/04/2023",name:"John",as_a:"Dev",
+      i_want_to:"be able to move tasks from product backlog to the scrum board",
+      so_that:"we can see what needs to be worked on.",priority:"2",sprint:"1",status:"In progress"},
+      {date:"23/04/2023",name:"Harry",as_a:"SM",
+      i_want_to:"be able to add and remove columns on my scrum board",
+      so_that:"we can update the scrum board.",priority:"1",sprint:"1",status:"Started"},
+      {date:"26/04/2023",name:"Josh",as_a:"PO",
+      i_want_to:"be able to assign one person to a task at a time",
+      so_that:"everyone is working one task at a time",priority:"3",sprint:"2",status:"Done"},
     ]
     return (
         <div>
@@ -16,6 +21,7 @@ const ProductBacklog: React.FunctionComponent<IPageProps> = props => {
         <table>
             <tbody>
                 <tr>
+                    <th>Date</th>
                     <th>Name</th>
                     <th>As a ...</th>
                     <th>I want to</th>
@@ -27,8 +33,10 @@ const ProductBacklog: React.FunctionComponent<IPageProps> = props => {
                 {data.map((val, key) => {
                 return (
                     <tr key = {key}>
+                    <td>{val.date}</td>
                     <td>{val.name}</td>
                     <td>{val.as_a}</td>
+                    <td>{val.i_want_to}</td>
                     <td>{val.so_that}</td>
                     <td>{val.priority}</td>
                     <td>{val.sprint}</td>
