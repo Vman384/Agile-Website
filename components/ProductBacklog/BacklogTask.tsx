703b0b8305b9108f16ef683ab92b50369db87e66
felixxx22 <felix.chunggg@gmail.com>
2023-09-27 15:40:23 +1000

    link backlog to databse

--- components/ProductBacklog/BacklogTask.tsx
+++ components/ProductBacklog/BacklogTask.tsx
@@ -1,32 +1,40 @@
 import { useState } from "react"
 
 export default function BacklogTask( 
-    {id, date, name, role, info, priority, sprint, status, taskChanged, provided} :
-        {id: string, date: string, name: string, role: string, info: string, priority: string, sprint: string, status: string} 
+    {id, date, name, type, info, estimate, tag, status, taskChanged, provided} :
+        {id: string, date: string, name: string, type: string, info: string, estimate: number, tag: string, status: string} 
     ) {
         const [dateVar, setDateVar] = useState(date)
         const [nameVar, setNameVar] = useState(name)
-        const [roleVar, setRoleVar] = useState(role)
+        const [typeVar, setTypeVar] = useState(type)
         const [infoVar, setInfoVar] = useState(info)
-        const [priorityVar, setPriorityVar] = useState(priority)
-        const [sprintVar, setSprintVar] = useState(sprint)
+        const [estimateVar, setEstimateVar] = useState(estimate)
+        const [tagVar, setTagVar] = useState(tag)
         const [statusVar, setStatusVar] = useState(status)
 
         return (
             <div className = "task">
                 <div {...provided.dragHandleProps} {...provided.draggableProps} ref = {provided.innerRef}>
                     <div className = "flex mx-10 h-fit justify-center p-6 bg-white border border-gray-200 rounded-lg shadow">
-                        <input className = "normal-width" type="text" placeholder="Date" onChange={(e) => {setDateVar(e.target.value); taskChanged(id, e.target.value, name, role, info, priority, sprint, status)}} value={dateVar}/>
-                        <input className = "normal-width"  type="text" placeholder="Name" onChange={(e) => {setNameVar(e.target.value); taskChanged(id, date, e.target.value, role, info, priority, sprint, status)}} value={nameVar}/>
-                        <input className = "normal-width"  type="text" placeholder="Role" onChange={(e) => {setRoleVar(e.target.value); taskChanged(id, date, name, e.target.value, info, priority, sprint, status)}} value={roleVar}/>
-                        <input className = "long-width"  type="text" placeholder="Information" onChange={(e) => {setInfoVar(e.target.value); taskChanged(id, date, name, role, e.target.value, priority, sprint, status)}} value={infoVar}/>
-                        <input className = "normal-width"  type="text" placeholder="Priority" onChange={(e) => {setPriorityVar(e.target.value); taskChanged(id, date, name, role, info, e.target.value, sprint, status)}} value={priorityVar}/>
-                        <select className = "normal-width"  name="Sprint" id="Sprint" onChange={(e) => {setSprintVar(e.target.value); taskChanged(id, date, name, role, info, priority, e.target.value, status)}} value={sprintVar}>
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
+                        <input className = "normal-width" type="text" placeholder="Date" onChange={(e) => {setDateVar(e.target.value); taskChanged(id, e.target.value, name, type, info, estimate, tag, status)}} value={dateVar}/>
+                        <input className = "normal-width"  type="text" placeholder="Name" onChange={(e) => {setNameVar(e.target.value); taskChanged(id, date, e.target.value, type, info, estimate, tag, status)}} value={nameVar}/>
+                        <select className = "normal-width"  name="Type" id="Type" onChange={(e) => {setTypeVar(e.target.value); taskChanged(id, date, name, type, info, estimate, e.target.value, status)}} value={typeVar}>
+                            <option>User Story</option>
+                            <option>Bug</option>
+                        </select>                        
+                        <input className = "long-width"  type="text" placeholder="Information" onChange={(e) => {setInfoVar(e.target.value); taskChanged(id, date, name, type, e.target.value, estimate, tag, status)}} value={infoVar}/>
+                        <input className = "normal-width"  type="Number" placeholder="Estimate" onChange={(e) => {setEstimateVar(e.target.valueAsNumber); taskChanged(id, date, name, type, info, e.target.value, tag, status)}} value={estimateVar}/>
+                        <select className = "normal-width"  name="Tag" id="Tag" onChange={(e) => {setTagVar(e.target.value); taskChanged(id, date, name, type, info, estimate, e.target.value, status)}} value={tagVar}>
+                            <option>Front end</option>
+                            <option>Back end</option>
+                            <option>API</option>
+                            <option>Database</option>
+                            <option>Framework</option>
+                            <option>Testing</option>
+                            <option>UI</option>
+                            <option>UX</option>
                         </select>
-                        <select className = "normal-width"  name="Status" id="Status" onChange={(e) => {setStatusVar(e.target.value); taskChanged(id, date, name, role, info, priority, sprint, e.target.value)}} value={statusVar}>
+                        <select className = "normal-width"  name="Status" id="Status" onChange={(e) => {setStatusVar(e.target.value); taskChanged(id, date, name, type, info, estimate, tag, e.target.value)}} value={statusVar}>
                             <option value="Task Backlog">Task Backlog</option>
                             <option value="In Progress">In Progress</option>
                             <option value="Awaiting Review">In Review</option>
--- src/app/ProductBacklog/page.tsx
+++ src/app/ProductBacklog/page.tsx
@@ -1,12 +1,22 @@
-"use client"
+"use client";
 
-import "../../../styles/product-backlog.css"
-import BacklogTask from "../../../components/ProductBacklog/BacklogTask"
+import "../../../styles/product-backlog.css";
+import BacklogTask from "../../../components/ProductBacklog/BacklogTask";
 import { useState } from "react";
-import React from "react";
-import {DragDropContext, Droppable, Draggable} from "react-beautiful-dnd"
+import React, { usestate, useEffect } from "react";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import { stat } from "fs";
-
+import {
+    collection,
+    addDoc,
+    getDocs,
+    querySnapshot,
+    query,
+    onSnapshot,
+    deleteDoc,
+    doc,
+} from "firebase/firestore";
+import { db } from "../../../config/firebaseSetup";
 
 const startingTasks = [
     {
@@ -15,8 +25,8 @@ const startingTasks = [
         name: "John",
         role: "Dev",
         info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque consequat at mauris vel volutpat. Duis viverra diam turpis, a aliquam justo maximus ac. ",
-        priority: "2",
-        sprint: "1",
+        estimate: "2",
+        // sprint: "1",
         status: "In progress",
     },
     {
@@ -25,8 +35,8 @@ const startingTasks = [
         name: "Harry",
         role: "SM",
         info: "Donec ac sagittis velit. Sed sit amet rhoncus nisl. Pellentesque posuere ultrices quam ut facilisis. Vivamus sagittis non ligula ut tempor.",
-        priority: "1",
-        sprint: "1",
+        estimate: "1",
+        // sprint: "1",
         status: "Started",
     },
     {
@@ -35,94 +45,160 @@ const startingTasks = [
         name: "Josh",
         role: "PO",
         info: "Sed maximus maximus diam at facilisis. Sed egestas mauris eu velit blandit venenatis.",
-        priority: "3",
-        sprint: "2",
+        estimate: "3",
+        // sprint: "2",
         status: "Done",
     },
 ];
 
-
 export default function ProductBacklog() {
-    
-    function taskChanged(id : string, date : string, name : string, role : string, info : string, priority : string, sprint : string, status : string) {
-        const indexOfTask = taskList.findIndex(task => task.id === id)
-        if (!(taskList[indexOfTask].id == id &&
-            taskList[indexOfTask].date == date &&
-            taskList[indexOfTask].name == name &&
-            taskList[indexOfTask].role == role &&
-            taskList[indexOfTask].info == info &&
-            taskList[indexOfTask].priority == priority &&
-            taskList[indexOfTask].sprint == sprint &&
-            taskList[indexOfTask].status == status)) {
-            taskList[indexOfTask] = {id: id, date: date, name: name, role: role, info: info, priority: priority, sprint: sprint, status: status}
-            console.log("changed")
+    // Read items from the database
+    const [taskList, setTaskList] = useState([]);
+
+    useEffect(() => {
+        const q = query(collection(db, "tasks"));
+        const unsubscribe = onSnapshot(q, (querySnapshot) => {
+           let tasksArr: (typeof taskList) = [];
+
+            querySnapshot.forEach((doc: any) => {
+                tasksArr.push({ ...doc.data(), id: doc.id });
+            });
+            setTaskList(tasksArr);
+            console.log("printing tasksArr")
+            console.log(tasksArr)
+            console.log("printing taskList")
+
             console.log(taskList)
+            return () => unsubscribe;
+        });
+    }, []);
+
+    function taskChanged(
+        id: string,
+        date: string,
+        name: string,
+        role: string,
+        info: string,
+        estimate: string,
+        sprint: string,
+        status: string
+    ) {
+        const indexOfTask = taskList.findIndex((task) => task.id === id);
+        if (
+            !(
+                taskList[indexOfTask].id == id &&
+                taskList[indexOfTask].date == date &&
+                taskList[indexOfTask].name == name &&
+                taskList[indexOfTask].role == role &&
+                taskList[indexOfTask].info == info &&
+                taskList[indexOfTask].estimate == estimate &&
+                taskList[indexOfTask].sprint == sprint &&
+                taskList[indexOfTask].status == status
+            )
+        ) {
+            taskList[indexOfTask] = {
+                id: id,
+                date: date,
+                name: name,
+                role: role,
+                info: info,
+                estimate: estimate,
+                sprint: sprint,
+                status: status,
+            };
+            console.log("changed");
+            console.log(taskList);
         }
     }
 
-
-    const [taskList, setTaskList] = useState(startingTasks);
-    
     return (
         <div>
             <div className="my-20 flex text-5xl font-extrabold justify-center items-center">
                 Sunday.com
             </div>
             <div>
-            {/* Header Row */}
-            <div className = "flex text-center mx-10 h-fit text-2xl justify-center p-6">
-                <div className = "normal-width">Date</div>
-                <div className = "normal-width">Name</div>
-                <div className = "normal-width">Role</div>
-                <div className = "long-width">Info</div>
-                <div className = "normal-width">Priority</div>
-                <div className = "normal-width">Sprint</div>
-                <div className = "normal-width">Status</div>
-            </div>
-            <DragDropContext onDragEnd={(results) => {
-                const {source, destination, type} = results
-                
-                if (!destination) return
-                if (source.index == destination.index) return
+                {/* Header Row */}
+                <div className="flex text-center mx-10 h-fit text-2xl justify-center p-6">
+                    <div className="normal-width">Date</div>
+                    <div className="normal-width">Name</div>
+                    <div className="normal-width">Type</div>
+                    <div className="long-width">Info</div>
+                    <div className="normal-width">Estimate</div>
+                    <div className="normal-width">Tag</div>
+                    <div className="normal-width">Status</div>
+                </div>
+                <DragDropContext
+                    onDragEnd={(results) => {
+                        const { source, destination, type } = results;
 
-                if (type === "group") {
-                    const reorderedTasks = [...taskList]
-                    const sourceIndex = source.index
-                    const destinationIndex = destination.index
-                    
-                    const [removedTask] = reorderedTasks.splice(sourceIndex, 1)
-                    reorderedTasks.splice(destinationIndex, 0, removedTask)
-                    
-                    return setTaskList(reorderedTasks)
-                }
+                        if (!destination) return;
+                        if (source.index == destination.index) return;
 
-            }}>
-                <Droppable droppableId = "ProductBacklog" type = "group">
-                    {(provided) => (
-                        <div {...provided.droppableProps} ref = {provided.innerRef}>
-                            {taskList.map((task : {id: string, date: string, name: string, role: string, info: string, priority: string, sprint: string, status: string}, index : number) => (
-                                <Draggable draggableId = {task.id} key = {task.id} index = {index}>
-                                    {(provided) => (
-                                        <BacklogTask 
-                                            id = {task.id}
-                                            date = {task.date}    
-                                            name = {task.name}
-                                            role = {task.role}
-                                            info = {task.info}    
-                                            priority = {task.priority}
-                                            sprint = {task.sprint}    
-                                            status = {task.status}
-                                            taskChanged = {taskChanged}
-                                            provided = {provided}
-                                            >
-                                        </BacklogTask>
-                                    )}
-                                </Draggable>
-                            ))}
-                        </div>
-                    )}
-                </Droppable>
-            </DragDropContext>
+                        if (type === "group") {
+                            const reorderedTasks = [...taskList];
+                            const sourceIndex = source.index;
+                            const destinationIndex = destination.index;
+
+                            const [removedTask] = reorderedTasks.splice(
+                                sourceIndex,
+                                1
+                            );
+                            reorderedTasks.splice(
+                                destinationIndex,
+                                0,
+                                removedTask
+                            );
+
+                            return setTaskList(reorderedTasks);
+                        }
+                    }}
+                >
+                    <Droppable droppableId="ProductBacklog" type="group">
+                        {(provided) => (
+                            <div
+                                {...provided.droppableProps}
+                                ref={provided.innerRef}
+                            >
+                                {taskList.map(
+                                    (
+                                        task: {
+                                            id: string;
+                                            date: string;
+                                            name: string;
+                                            type: string;
+                                            sprint: string;
+                                            info: string;
+                                            estimate: string;
+                                            status: string;
+                                        },
+                                        index: number
+                                    ) => (
+                                        <Draggable
+                                            draggableId={task.id}
+                                            key={task.id}
+                                            index={index}
+                                        >
+                                            {(provided) => (
+                                                <BacklogTask
+                                                    id={task.id}
+                                                    date={task.date}
+                                                    name={task.name}
+                                                    type={task.type}
+                                                    sprint={task.sprint}
+                                                    info={task.info}
+                                                    estimate={task.estimate}
+                                                    status={task.status}
+                                                    taskChanged={taskChanged}
+                                                    provided={provided}
+                                                ></BacklogTask>
+                                            )}
+                                        </Draggable>
+                                    )
+                                )}
+                            </div>
+                        )}
+                    </Droppable>
+                </DragDropContext>
             </div>
         </div>
     );
