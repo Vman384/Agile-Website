cb1fad47a18f9a267f69e0786197952104b6c7d8
TamvyQuan <quantam052@gmail.com>
2023-10-18 21:43:12 +1100

    Working new scrum tasks
    sineths code merged

--- src/app/ScrumBoard/components/TaskCard.js
+++ src/app/ScrumBoard/components/TaskCard.js
@@ -1,13 +1,15 @@
-dButton from "./TaskCardButton";
+import CardButton from "./TaskCardButton";
 import {DragDropContext, Draggable, Droppable} from 'react-beautiful-dnd';
 
 
 const TaskCard = (props) => {
     const { data, index } = props;
   
-    if (!data || !data.name) {
+    if (!data || !data.text) {
       return null; // Handle the case where data is undefined or does not have a text property
     }
+    
+    console.log(data)
   
     return (
     <Draggable index={index} draggableId={data.id.toString()}>
@@ -22,23 +24,23 @@ const TaskCard = (props) => {
             className={`bg-gradient-to-r
               px-2 py-1 rounded text-white text-sm
               ${
-                data.priority === 0
+                data.priority === 3
                   ? "from-blue-600 to-blue-400"
-                  : data.priority === 1
+                  : data.priority === 4
                   ? "from-green-600 to-green-400"
                   : "from-red-600 to-red-400"
               }
               `}
           >
-            {data.priority === 0
+            {data.priority === 3
               ? "Low Priority"
-              : data.priority === 1
+              : data.priority === 4
               ? "Medium Priority"
               : "High Priority"}
           </label>
       <div className="block w-100 mx-10 h-fit m-auto p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
         <h2 className="text-xl dark:text-white"
-        style={{fontWeight: "bold"}}
+          style={{fontWeight: "bold"}}
         >{data.name}</h2>
         <p className="m-auto text-black-500 dark:text-gray-400">{data.description}</p>
         <p className="m-auto text-gray-500 dark:text-gray-400">{data.tag}</p>
@@ -53,8 +55,7 @@ const TaskCard = (props) => {
         color: '#fff',
         fontSize: '12px',
         fontWeight: 'bold'}}
-        >{data.estimate}</p>
-        {/* <CardButton card={data} /> */}
+        >{data.priority}</p>
       </div>
       </div>
       )}
